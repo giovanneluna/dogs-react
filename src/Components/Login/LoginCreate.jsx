211930df211abf51213950a4a24c2cb349600cf4
Giovanne de Luna <gikalunalticg@gmail.com>
2024-04-25 16:46:37 -0300

    style components input, button

---
+++ src/Components/Login/LoginCreate.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const LoginCreate = () => {
+  return <div>Cadastrar</div>
+}
+
+export default LoginCreate
---
+++ src/Components/Login/LoginPasswordRest.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const LoginPasswordRest = () => {
+  return <div>LoginPasswordRest</div>
+}
+
+export default LoginPasswordRest
